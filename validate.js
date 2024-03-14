ddfd2fc5c7344ad710b7b8e52d776e5b99b0f032
bl0utooth <136910180+bl0utooth@users.noreply.github.com>
2024-03-14 16:35:34 -0400

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,9 @@
+function validate(s) {
+    if (s === 0 || s === 1) {
+        return true;
+    }
+    if(Array.isArray(s) && s.length === 4) {
+        return s.every(validate);
+    }
+    return false;
+}
